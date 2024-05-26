416709d21013b5f6dd6e525bc05d4717529cc213
Daniel Pritsker <dani.pritsker@gmail.com>
2024-05-26 09:12:17 +0300

    init project

---
+++ src/main/java/com/niq/interview/niq_interview/NiqInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.niq.interview.niq_interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiqInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiqInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niq/interview/niq_interview/NiqInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niq.interview.niq_interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiqInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
